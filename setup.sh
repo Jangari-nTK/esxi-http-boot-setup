8ad5e80f1696047a7d157e3c6ab8ddefe3556517
Jangari-nTK <Jangari-nTK@users.noreply.github.com>
2021-08-08 02:56:40 +0900

    modify the path of ks.cfg

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
-IP_ADDR=`ip addr show dev ens192 | grep "inet " | sed -r "s#.*inet (.*)/.*#\1#"`
 DIR_NAME=esxi70u2
+INTERFACE=ens192
 
+IP_ADDR=`ip addr show dev $INTERFACE | grep "inet " | sed -r "s#.*inet (.*)/.*#\1#"`
+BOOT_IMAGE_DIR=/var/www/html/$DIR_NAME
+BOOT_CFG=$BOOT_IMAGE_DIR/efi/boot/boot.cfg
+BOOT_PREFIX=http://$IP_ADDR/$DIR_NAME
+
+# Setup httpd
+yum install -y httpd
+firewall-cmd --add-service=http --permanent
+firewall-cmd --reload
+systemctl enable --now httpd
+
+# Setup directory for boot image
 mkdir /mnt/cdrom
 mount /dev/cdrom /mnt/cdrom
-
-BOOT_IMAGE_DIR=/var/www/html/$DIR_NAME
 mkdir $BOOT_IMAGE_DIR
 cp -R /mnt/cdrom/* $BOOT_IMAGE_DIR
-
 chmod -R 755 $BOOT_IMAGE_DIR
 restorecon -Rv $BOOT_IMAGE_DIR
 
-BOOT_CFG=$BOOT_IMAGE_DIR/efi/boot/boot.cfg
+# Configure parameters in boot.cfg
 sed -i "s#/##g" $BOOT_CFG
-
-BOOT_PREFIX=http://$IP_ADDR/$DIR_NAME
 sed -i "s#prefix=#prefix=$BOOT_PREFIX#" $BOOT_CFG
-sed -i "s#kernelopt=.*#kernelopt=ks=$BOOT_PREFIX/efi/boot/ks.cfg allowLegacyCPU=true#" $BOOT_CFG
+sed -i "s#kernelopt=.*#kernelopt=ks=$BOOT_PREFIX/ks.cfg allowLegacyCPU=true#" $BOOT_CFG
 
-cp ks.cfg $BOOT_IMAGE_DIR/efi/boot/ks.cfg
+# Copy ks.cfg
+cp ks.cfg $BOOT_IMAGE_DIR/ks.cfg
